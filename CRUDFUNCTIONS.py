3cd1cbd9162ad5ce03caa694678c19d5f487cb8f
Kaishin504 <kinrintenseibaku@gmail.com>
2026-06-06 16:19:40 -0300

    changes: created list_categories function

--- CRUDFUNCTIONS.py
+++ CRUDFUNCTIONS.py
@@ -41,6 +41,15 @@ def list_products(cursor):
         product_list.append({"ID":id,"NAME":name,"CATEGORY_NAME":category_name,"CATEGORY_ID":categoryFK,"PRICE":price,"STOCK":stock})
     return product_list
 
+def list_categories(cursor):
+    products = cursor.execute("SELECT * FROM ProductCategory")
+    fetch = products.fetchall()
+    product_list = []
+    for item in fetch:
+        id,category_name = item
+        product_list.append({"ID":id,"CATEGORY_NAME":category_name})
+    return product_list
+
 def search_product(cursor,id_or_name):
     try:
         product_id_search = cursor.execute("""
