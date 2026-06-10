4ea35b902201c66aaf2ba1e4b8be50bbf7200a54
Kaishin504 <kinrintenseibaku@gmail.com>
2026-06-09 21:05:13 -0300

    changes: standardized the error text, made all letters become uppercase (for looks only). bugfixes: if list products or list categories or view stock log have no item on the database, they should pop a message saying no <object> found. stock out option wont allow you to take out more stock than there is.

--- CRUDFUNCTIONS.py
+++ CRUDFUNCTIONS.py
@@ -3,21 +3,22 @@ def create_product(cursor,name,category,price,stock):
     product_name_validation = cursor.execute("SELECT ProductName FROM Products WHERE ProductName = ?",(name,))
     results = product_name_validation.fetchone()
     if results is not None:
-        raise ValueError(f"ERROR: Product {results[0]} Already Exists!")
+        raise ValueError(f"PRODUCT {results[0]} ALREADY EXISTS")
     category_validation = cursor.execute("SELECT CategoryPK FROM ProductCategory WHERE CategoryName = ?",(category,))
     results = category_validation.fetchone()
     if results is None:
-        raise ValueError("ERROR: Category Not Found!")
+        raise ValueError("CATEGORY NOT FOUND")
     else:
         categoryFK = results[0]
     if price <= 0:
-        raise ValueError("ERROR: Price Cannot Be Less Than/Equal to 0!")
+        raise ValueError("PRICE CANNOT BE LESS THAN/EQUAL TO 0")
     if stock <= 0:
-        raise ValueError("ERROR: Stock Cannot Be Less Than/Equal to 0!")
+        raise ValueError("STOCK CANNOT BE LESS THAN/EQUAL TO 0")
+    name = name.upper()
     cursor.execute("INSERT INTO Products (ProductName,CategoryFK,ProductPrice,ProductStock) VALUES (?,?,?,?)",(name,categoryFK,price,stock))
     search = cursor.execute("SELECT ProductPK FROM Products WHERE ProductName = ?",(name,))
     fetch = search.fetchone()
-    return [{"MESSAGE":"Product Successfully Created. "},{"ID":fetch[0]}]
+    return [{"MESSAGE":"PRODUCT SUCCESSFULLY CREATED "},{"ID":fetch[0]}]
     
     
 
@@ -25,13 +26,14 @@ def create_category(cursor,category_name):
     category_validation = cursor.execute("SELECT CategoryPK FROM ProductCategory WHERE CategoryName = ?",(category_name,))
     fetch = category_validation.fetchone()
     if fetch is not None:
-        raise ValueError("ERROR: Category Already Exists!")
+        raise ValueError("CATEGORY ALREADY EXISTS")
     else:
+        category_name = category_name.upper()
         cursor.execute("INSERT INTO ProductCategory (CategoryName) VALUES (?)",(category_name,))
         search = cursor.execute("SELECT CategoryPK FROM ProductCategory WHERE CategoryName = ?",(category_name,))
         fetch = search.fetchone()
         category_id = fetch[0]
-        return ([{"MESSAGE":f"Created Category {category_name}!"},{"CATEGORY_ID":category_id}])
+        return ([{"MESSAGE":f"CREATED CATEGORY {category_name}"},{"CATEGORY_ID":category_id}])
 
 
          
@@ -45,6 +47,8 @@ def list_products(cursor):
         fetch2 = category.fetchone()
         category_name = fetch2[0]
         product_list.append({"ID":id,"NAME":name,"CATEGORY_NAME":category_name,"CATEGORY_ID":categoryFK,"PRICE":price,"STOCK":stock})
+    if len(product_list) == 0:
+        raise ValueError("NO PRODUCT FOUND")
     return product_list
 
 
@@ -56,6 +60,8 @@ def list_categories(cursor):
     for item in fetch:
         id,category_name = item
         product_list.append({"ID":id,"CATEGORY_NAME":category_name})
+    if len(product_list) == 0:
+        raise ValueError("NO CATEGORY FOUND")
     return product_list
 
 
@@ -82,7 +88,7 @@ def search_product(cursor,id_or_name):
         category_name,id,name,category_fk,price,stock = results
         return({"ID":id,"NAME":name,"CATEGORY_NAME":category_name,"CATEGORY_ID":category_fk,"PRICE":price,"STOCK":stock})
     else:
-        raise ValueError("ERROR: Product Not Found!")
+        raise ValueError("PRODUCT NOT FOUND")
        
         
 
@@ -101,29 +107,30 @@ def update_product(cursor,id,name=None,category=None,price=None):
         name_verification = cursor.execute("SELECT ProductName FROM Products WHERE ProductName = ?",(name,))
         results = name_verification.fetchone()
         if results is not None:
-            raise ValueError(f"ERROR: Product {results[0]} Already Exists!")
+            raise ValueError(f"PRODUCT {results[0]} ALREADY EXISTS")
         name = name.lower()
         info_list.update({"NAME":name})
     if category is not None:
         category_verification = cursor.execute("SELECT CategoryPK FROM ProductCategory WHERE CategoryName = ?",(category.lower(),))
         if category_verification.fetchone() == None:
-            raise ValueError("ERROR: Category Does Not Exist!")
+            raise ValueError("CATEGORY DOES NOT EXIST")
         else:
             info_list.update({"CATEGORY":category_verification.fetchone()})
     if price is not None:
         if price <= 0:
-            raise ValueError("ERROR: Price Cannot Be Lower Than/Equal To 0")
+            raise ValueError("PRICE CANNNOT BE LOWER THAN/EQUAL TO 0")
         else:
             info_list.update({"PRICE":price})
     if name == None and category == None and price == None:
-        raise ValueError("ERROR: You Must Give At Least One Argument To Update!")
+        raise ValueError("YOU MUST GIVE AT LEAST ONE ARGUMENT TO UPDATE")
     if info_list.get("NAME"):
+        info_list["NAME"] = info_list["NAME"].upper()
         cursor.execute("UPDATE Products SET ProductName = ? WHERE ProductPK = ?",(info_list["NAME"],id))
     if info_list.get("CATEGORY"):
         cursor.execute("UPDATE Products SET CategoryFK = ? WHERE ProductPK = ?",(category,id))
     if info_list.get("PRICE"):
         cursor.execute("UPDATE Products SET ProductPrice = ? WHERE ProductPK = ?",(info_list["PRICE"],id))
-    return "Product Updated Successfully."
+    return "PRODUCT UPDATED SUCCESSFULLY"
 
 
 
@@ -131,9 +138,9 @@ def delete_product(cursor,id):
     search = cursor.execute("SELECT * FROM Products WHERE ProductPK = ?", (id,))
     product = search.fetchone()
     if product is None:
-        raise ValueError("ERROR: Product Does Not Exist!")
+        raise ValueError("PRODUCT DOES NOT EXIST")
     cursor.execute("DELETE FROM Products WHERE ProductPK = ?", (id,))
-    return "Product Successfully Deleted."
+    return "PRODUCT SUCCESSFULLY DELETED"
 
 
 
@@ -141,21 +148,21 @@ def delete_category(cursor,id):
     search = cursor.execute("SELECT * FROM ProductCategory WHERE CategoryPK = ?",(id,))
     product = search.fetchone()
     if product is None:
-        raise ValueError("ERROR: Category Does Not Exist!")
+        raise ValueError("CATEGORY DOES NOT EXIST")
     cursor.execute("DELETE FROM ProductCategory WHERE CategoryPK = ?",(id,))
-    return "Category Successfully Deleted."
+    return "CATEGORY SUCCESSFULLY DELETED"
 
 def stock_in(cursor,id,amount):
     search = cursor.execute("SELECT ProductStock FROM Products WHERE ProductPK = ?",(id,))
     fetch = search.fetchone()
     if fetch is None:
-        raise ValueError("ERROR: Product Does Not Exist!")
+        raise ValueError("PRODUCT DOES NOT EXIST")
     if amount <= 0:
-        raise ValueError("ERROR: Stock-in Cannot Be Lower Than/Equal To 0!")
+        raise ValueError("STOCK-IN CANNOT BE LOWER THAN/EQUAL TO 0")
     current_stock = fetch[0]
     new_stock = current_stock + amount
     cursor.execute("UPDATE Products SET ProductStock = ? WHERE ProductPK = ?",(new_stock,id))
-    return "Stock-In Registered."
+    return "STOCK-IN REGISTERED"
 
 
 
@@ -163,19 +170,23 @@ def stock_out(cursor,id,amount):
     search = cursor.execute("SELECT ProductStock FROM Products WHERE ProductPK = ?",(id,))
     fetch = search.fetchone()
     if fetch == None:
-        raise ValueError("ERROR: Product Does Not Exist!")
+        raise ValueError("PRODUCT DOES NOT EXIST")
     if amount == 0: 
-        raise ValueError("ERROR: Quantity Added Cannot Be 0!")
+        raise ValueError("QUANTITY ADDED CANNOT BE 0")
     if amount > 0:
         current_stock = fetch[0]
         new_stock = current_stock - amount
+        if new_stock < 0:
+            raise ValueError("INSUFFICIENT STOCK")
         cursor.execute("UPDATE Products SET ProductStock = ? WHERE ProductPK = ?",(new_stock,id))
-        return "Stock-Out Registered."
+        return "STOCK-OUT REGISTERED"
     if amount < 0:
         current_stock = fetch[0]
         new_stock = current_stock + amount
+        if new_stock < 0:
+            raise ValueError("INSUFFICIENT STOCK")
         cursor.execute("UPDATE Products SET ProductStock = ? WHERE ProductPK = ?",(new_stock,id))
-        return "Stock-Out Registered."
+        return "STOCK-OUT REGISTERED"
     
 def stock_log(cursor,id,amount,type):
     search = cursor.execute("SELECT ProductName FROM Products WHERE ProductPK = ?",(id,))
@@ -195,11 +206,13 @@ def system_log(cursor,entity,entity_name,id,operation_type):
 def view_system_log(cursor):
     system_log = cursor.execute("SELECT * FROM SystemLog")
     fetch = system_log.fetchall()
-    product_list = []
+    system_list = []
     for item in fetch:
         id,entity,entity_name,entity_id,operation_type,date = item
-        product_list.append({"ID":id,"ENTITY":entity,"ENTITY_NAME":entity_name,"ENTITY_ID":entity_id,"OPERATION_TYPE":operation_type,"DATE":date})
-    return product_list
+        system_list.append({"ID":id,"ENTITY":entity,"ENTITY_NAME":entity_name,"ENTITY_ID":entity_id,"OPERATION_TYPE":operation_type,"DATE":date})
+    if len(system_list) == 0:
+        raise ValueError("LOG IS EMPTY")
+    return system_list
 
 def view_stock_log(cursor):
     stock_log = cursor.execute("SELECT * FROM StockLog")
@@ -208,4 +221,6 @@ def view_stock_log(cursor):
     for item in fetch:
         id,name,productFK,amount,movimentation_type,date = item
     product_list.append({"ID":id,"NAME":name,"PRODUCT_ID":productFK,"AMOUNT":amount,"MOVIMENTATION_TYPE":movimentation_type,"OPERATION_DATE":str(date)})
+    if len(product_list) == 0:
+        raise ValueError("LOG IS EMPTY")
     return product_list
